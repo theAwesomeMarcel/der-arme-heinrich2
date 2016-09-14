706e0b8eaabe164bdc984ff82f7ea6d236320f4e
theAwesomeMarcel <marcel.kemmerich@gmail.com>
2016-09-14 23:39:42 +0200

    Player und Background

---
+++ Der arme Heinrich/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        // float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveSpeed * moveHorizontal, 0.0f);
+
+        myRigidbody.velocity = movement;
+
+    }
+}
