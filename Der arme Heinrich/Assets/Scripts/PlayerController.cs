9101b151c8ac37e030c9401f228b8648ca54cd57
theAwesomeMarcel <marcel.kemmerich@gmail.com>
2016-09-16 14:58:14 +0200

    Pick Ups

--- Der arme Heinrich/Assets/Scripts/PlayerController.cs
+++ Der arme Heinrich/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private Rigidbody2D myRigidbody;
 
-    // Use this for initialization
+
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
@@ -24,4 +24,9 @@ public class PlayerController : MonoBehaviour
         myRigidbody.velocity = movement;
 
     }
+
+    void Update() {
+
+    }
+
 }
